53b290c332e0658cd79ddc5574552d2b74db2f2f
Daniel Christine <danieljameschristine@gmail.com>
2021-02-10 20:59:04 -0600

    images for carousel added. New wireframe added. Changed to horizontal navbar. Added css styling for carousel.

---
+++ main.js
@@ -0,0 +1,16 @@
+var bgColor = "rgb(" + 158 + "," + 172 + "," + 138 + ")";
+console.log(bgColor);
+
+document.body.style.background = bgColor;
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+  });
